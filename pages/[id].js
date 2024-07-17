d07407ff54642e0c7861bf5fc3a73096ba8cdba0
Jessica Lörs <jessica.loers@gmail.com>
2024-07-17 15:54:52 +0200

    initial

---
+++ pages/[id].js
@@ -0,0 +1,52 @@
+import useSWR from 'swr';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import JokeForm from '@/components/JokeForm';
+
+export default function JokeDetailPage() {
+  //🆕🆕🆕
+  const [isEditMode, setIsEditMode] = useState(false);
+  const router = useRouter();
+  const { id } = router.query;
+
+  const { data, isLoading } = useSWR(`/api/jokes/${id}`);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (!data) return;
+
+  // 🆕🆕🆕
+  function handleEdit(event) {
+    event.preventDefault();
+  }
+
+  // 🆕🆕🆕
+  async function handleDelete() {}
+
+  return (
+    <>
+      <small>ID: {id}</small>
+      <h1>{data.joke} </h1>
+      <div>
+        <button
+          onClick={() => {
+            setIsEditMode(!isEditMode);
+          }}>
+          <span role="img" aria-label="A pencil">
+            ✏️
+          </span>
+        </button>
+        <button onClick={handleDelete} disabled={isEditMode}>
+          <span role="img" aria-label="A cross indicating deletion">
+            ❌
+          </span>
+        </button>
+      </div>
+      {isEditMode && <JokeForm onSubmit={handleEdit} value={data.joke} isEditMode={true} />}
+      <Link href="/">Back to all</Link>
+    </>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,49 @@
+import useSWR from 'swr';
+import Link from 'next/link';
+import JokeForm from '../components/JokeForm';
+
+export default function HomePage() {
+  const { data, isLoading, mutate } = useSWR('/api/jokes');
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (!data) {
+    return;
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const jokeData = Object.fromEntries(formData);
+
+    const response = await fetch('/api/jokes', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(jokeData),
+    });
+
+    if (response.ok) {
+      mutate();
+    }
+
+    event.target.reset();
+  }
+
+  return (
+    <>
+      <JokeForm onSubmit={handleSubmit} value="" />
+      <ul>
+        {data.map((joke) => (
+          <li key={joke._id}>
+            <Link href={`/${joke._id}`}>{joke.joke}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
